11278aa8ea8d424489ec95386bcfd17e109bb23b
clydekeanu-rgb <abenojarclydekeanu@gmail.com>
2026-07-01 14:06:31 +0800

    fix merge conflict

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,11 +7,6 @@ import Landing from "@/pages/Landing";
 import Login from "@/pages/Login";
 import Signup from "@/pages/Signup";
 import Pricing from "@/pages/Pricing";
-<<<<<<< HEAD
-=======
-import ForgotPassword from "@/pages/ForgotPassword";
-import UpdatePassword from "@/pages/UpdatePassword";
->>>>>>> 9a99d29858d4e1a91ddcfbb56bc3cdd87750566c
 import Dashboard from "@/pages/Dashboard";
 import ProjectWorkspace from "@/pages/ProjectWorkspace";
 
@@ -25,11 +20,6 @@ function App() {
             <Route path="/login" element={<Login />} />
             <Route path="/signup" element={<Signup />} />
             <Route path="/pricing" element={<Pricing />} />
-<<<<<<< HEAD
-=======
-            <Route path="/forgot-password" element={<ForgotPassword />} />
-            <Route path="/update-password" element={<UpdatePassword />} />
->>>>>>> 9a99d29858d4e1a91ddcfbb56bc3cdd87750566c
             <Route
               path="/dashboard"
               element={
